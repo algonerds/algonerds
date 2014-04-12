5b3ce0ce90fc2800fc9e83387313e6c174440d45
Sergey Enin <sergeyenin@gmail.com>
2014-04-12 11:41:56 +0300

    Binary Search by Sergey

--- BinarySearch/src/BinarySearch.java
+++ BinarySearch/src/BinarySearch.java
@@ -1,5 +1,19 @@
 public class BinarySearch {
     public static int find(int[] array, int element) {
-        return -1;
+        return find(array, element, 0, array.length-1);
+    }
+
+    public static int find(int[] array, int element, int left, int right) {
+        int mid = (right-left)/2;
+        if (left>=right) return -1;
+        if (element < array[mid]) {
+            return find(array, element, left, mid);
+        }
+        else if (element > array[mid]) {
+            return find(array, element, mid, right);
+        }
+        else {
+            return mid;
+        }
     }
 }
