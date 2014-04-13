43f6ffd18f5afe5149f7b17c640195faf82898a1
Alexey Tarasevich <Alexey_Tarasevich@epam.com>
2014-04-13 20:39:25 +0300

    more tests

--- BinarySearch/src/BinarySearchTestTest.java
+++ BinarySearch/src/BinarySearchTestTest.java
@@ -34,20 +34,32 @@ public class BinarySearchTestTest {
         int[] array  = {};
         assertEquals(-1,BinarySearch.find(array, 1));
     }
-    
+
     @Test
     public void findElementInOneElementArray()
     {
         int[] array  = {8};
         assertEquals(0,BinarySearch.find(array, 8));
     }
-    
+
     @Test
     public void findElementInsideArray()
     {
         int[] array  = {1,2,3,4,5,6,7,8,9,10,11};
         assertEquals(8,BinarySearch.find(array, 9));
     }
-    
 
+    @Test
+    public void findFirstOfTwo() {
+      assertEquals(0, BinarySearch.find(a(1, 2), 1));
+    }
+
+    @Test
+    public void findSecondOfTwo() {
+      assertEquals(1, BinarySearch.find(a(1, 2), 2));
+    }
+
+    private static int[] a(int... array) {
+      return array;
+    }
 }
