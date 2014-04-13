e351b5a9a60a09040afd70f08191380de12024be
Sergey Enin <sergeyenin@gmail.com>
2014-04-14 00:12:22 +0300

    Implemented Iterative version as well

--- BinarySearch/src/BinarySearch.java
+++ BinarySearch/src/BinarySearch.java
@@ -1,19 +1,42 @@
 public class BinarySearch {
     public static int find(int[] array, int element) {
-        return find(array, element, 0, array.length-1);
+        int iterativeResult = findIterative(array,  element);
+        int reccursiveResult = findReccursive(array, element, 0, array.length-1);
+        int resultValue = (iterativeResult == reccursiveResult) ? iterativeResult :  -1;
+
+        return resultValue;
     }
 
-    public static int find(int[] array, int element, int left, int right) {
+    public static int findIterative(int[] array, int element){
+        int left = 0, right = array.length-1;
+        int mid = 0;
+        while (left <= right) {
+            mid = left +(right-left)/2;
+            if (element == array[mid]){
+                return mid;
+            }
+            else if (element > array[mid]){
+                left = mid + 1;
+            }
+            else if (element < array[mid]){
+                right = mid - 1;
+            }
+
+        }
+        return -1;
+    }
+
+    public static int findReccursive(int[] array, int element, int left, int right) {
         if (left > right) return -1;
         int mid = left+(right-left)/2;
         if (element == array[mid]){
             return mid;
         }
         else if (element < array[mid]) {
-            return find(array, element, left, mid-1);
+            return findReccursive(array, element, left, mid - 1);
         }
         else if (element > array[mid]) {
-            return find(array, element, mid+1, right);
+            return findReccursive(array, element, mid + 1, right);
         }
         return -1;
     }
