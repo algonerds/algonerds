ce5e2c2c36a60ef4bca51c3f7a478daf41c42bdc
Den Sergeyev <2.den.sergeyev@gmail.com>
2014-04-16 01:48:15 +0300

    Adding iterative binary search.

--- BinarySearch/src/BinarySearch.java
+++ BinarySearch/src/BinarySearch.java
@@ -1,5 +1,16 @@
 public class BinarySearch {
     public static int find(int[] array, int element) {
-        return -1;
+      int left = 0;
+      int right = array.length - 1;
+
+      while (left <= right){
+        int mid = left + (right - left)/2;
+
+        if (array[mid] < element)       left = mid + 1;
+        else if (array[mid] > element)  right = mid - 1;
+        else                            return mid;
+      }
+
+      return -1;
     }
 }
