5392447ca5323a929d463c7c35f3936e10cbb696
Alexey Tarasevich <Alexey_Tarasevich@epam.com>
2014-04-27 12:45:23 +0300

    Selection Sort

--- Sorting/src/SelectionSort.java
+++ Sorting/src/SelectionSort.java
@@ -1,8 +1,19 @@
-/**
- * Created by Egor Hamaliy on 4/21/14.
- */
+import java.util.Arrays;
+
 public class SelectionSort implements Sortable {
     public int[] sort(int[] numbers) {
-        return null;
+        int[] a = Arrays.copyOf(numbers, numbers.length);
+        int l = numbers.length;
+
+        for (int i = 0; i <= l-2; ++i) { // beginning of the range
+            int m = i; // index of the smallest
+            for (int j = i+1; j < l; ++j) { // elements in the range but the first
+                if (a[j] < a[m])
+                    m = j;
+            }
+            int t = a[m]; a[m] = a[i]; a[i] = t;
+        }
+
+        return a;
     }
 }
