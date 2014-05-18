ea50f1af1152193db5a8e11cf3452741f6ea41bb
Alexey Tarasevich <Alexey_Tarasevich@epam.com>
2014-05-18 13:44:35 +0300

    top-down merge sort of an array

--- Sorting/src/SortingTest.java
+++ Sorting/src/SortingTest.java
@@ -31,6 +31,7 @@ public class SortingTest {
     @Parameterized.Parameters(name = "#{index}: {0}")
     public static Collection data() {
         Class[][] data = {
+          {TopDownMergeSort.class},
           {BottomUpMergeSort.class},
           {BottomUpListMergeSort.class},
           {InsertionSort.class},
